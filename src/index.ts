9e5e7b9a6abb5c0ea8ef4b59d2900256d4243787
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-09-22 15:11:02 +0530

    add zod validation on signup route
    refactor code

--- src/index.ts
+++ src/index.ts
@@ -2,42 +2,10 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import {
-  userModel,
-  contentModel,
-  linkModel,
-  tagModel,
-} from "./models/models.js";
-const salt = process.env.SALT;
-if (!salt) process.exit(1);
-const saltRounds = parseInt(salt, 10);
+import { signup } from "./middleware/signup.js";
 const app = express();
 app.use(express.json());
-app.post("/api/v1/signup", async (req, res) => {
-  console.log(req.body);
-  try {
-    let { username, password, email } = req.body;
-    password = await bcrypt.hash(password, saltRounds);
-    const newUser = new userModel({
-      username,
-      password,
-      email,
-    });
-    await newUser.save();
-    res.json({
-      message: "success",
-    });
-  } catch (err) {
-    // THIS IS THE MOST IMPORTANT STEP
-    console.error("SIGNUP FAILED:", err);
-
-    // You can then send a generic error to the client
-    res.status(500).json({
-      message: "Failed to create user.",
-    });
-  }
-});
+app.post("/api/v1/signup", signup);
 
 app.post("/api/v1/signin", (req, res) => {});
 
@@ -55,9 +23,7 @@ async function main() {
   await mongoose.connect(`${process.env.MONGO_URL}`);
   console.log("DB conneccted");
 }
-
 main();
-
 app.listen(process.env.PORT, (err) => {
   if (err) console.log(err.message);
   else console.log(`http://localhost:${process.env.PORT}`);
---
+++ src/middleware/signup.ts
@@ -0,0 +1,42 @@
+import type { Request, Response } from "express";
+import { userModel } from "../models/models.js";
+import bcrypt from "bcrypt";
+import { validUser } from "../validator/signin.js";
+import { MongoServerError } from "mongodb";
+const salt = process.env.SALT;
+if (!salt) process.exit(1);
+const saltRounds = parseInt(salt, 10);
+export async function signup(req: Request, res: Response) {
+  const isValidUser = validUser.safeParse(req.body);
+  if (isValidUser.success) {
+    try {
+      let { username, password, email } = req.body;
+      const hashedPassword: string = await bcrypt.hash(password, saltRounds);
+      const newUser = new userModel({
+        username,
+        password: hashedPassword,
+        email,
+      });
+      await newUser.save();
+      res.json({
+        message: "success",
+      });
+    } catch (err) {
+      console.log(err);
+      if (err instanceof MongoServerError && err.code == 11000) {
+        return res.json({
+          message: "email already used",
+        });
+      }
+      res.status(500).json({
+        message: "failed to create user.",
+      });
+    }
+  } else {
+    const errorMessage = isValidUser.error.issues
+      .map((issue) => `${issue.path.join(".")}: ${issue.message}`)
+      .join(". ");
+
+    return res.status(400).json({ message: `Invalid input. ${errorMessage}` });
+  }
+}
