95408d91198a51829f2a42228111ef528aa0048b
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-09-29 12:03:08 +0530

    implemented createLink controller

---
+++ src/controller/createLink.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import { nanoid } from "nanoid";
+import { contentModel, linkModel } from "../models/models.js";
+
+export async function shareLink(req: Request, res: Response) {
+  try {
+    const contentId = req.params.id;
+    //@ts-ignore
+    const userId = req.user.userId;
+    const content = await contentModel.findOne({
+      _id: contentId,
+      userId,
+    });
+    if (!content) {
+      return res.status(404).json({
+        message: "Content not found or you don't have permission to share it.",
+      });
+    }
+    const alreadyPresent = await linkModel.findOne({
+      contentId,
+    });
+
+    if (alreadyPresent) {
+      const hash = alreadyPresent.hash;
+      const sharableLink = `${process.env.SERVER_URL}/api/v1/brain/${hash}`;
+      return res.status(200).json({
+        message: sharableLink,
+      });
+    }
+    const hash = await nanoid(10);
+    const newLink = new linkModel({
+      hash,
+      contentId,
+    });
+    await newLink.save();
+    const sharableLink = `${process.env.SERVER_URl}/api/v1/brain/${hash}`;
+    res.status(201).json({
+      message: sharableLink,
+    });
+  } catch (err) {
+    console.log("Error creating link: " + err);
+    res.status(500).json({
+      message: "Internet server error",
+    });
+  }
+}
