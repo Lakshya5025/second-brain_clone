2e20e0b4830fa24d3a25107b627a16fbdd0e991a
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-09-29 13:36:47 +0530

    initialize frontend

--- src/validator/signin.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-export const signinValidator = z.object({
-  username: z.string("username must be string"),
-  password: z.string("password must be string"),
-});
--- src/validator/signup.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "zod";
-
-export const validUser = z.object({
-  username: z.string("username must be string"),
-  password: z.string("password must be string"),
-  email: z.email("provide valid email"),
-});
