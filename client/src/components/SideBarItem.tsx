05cb02ba0c693aa3c9e39cb6f19a550ec5992d0f
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-09-30 15:40:12 +0530

    create SideBarItem component

---
+++ client/src/components/SideBarItem.tsx
@@ -0,0 +1,19 @@
+import type { ReactElement } from "react";
+
+interface SideBarItemPropType {
+  name: string;
+  logo: ReactElement;
+}
+
+export function SideBarItem({ name, logo }: SideBarItemPropType) {
+  return (
+    <div className=" mx-1 mb-1 flex items-center text-black-500 p-3 font-medium box-content hover:bg-black-200 hover:cursor-pointer rounded-md">
+      <div className="text-slate-950">
+        <div>{logo}</div>
+      </div>
+      <div>
+        <div className="ml-9 text-lg">{name}</div>
+      </div>
+    </div>
+  );
+}
