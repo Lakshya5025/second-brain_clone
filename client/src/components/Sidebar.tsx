39985019a9a1e5b52f821adda4ac852b3bbbd084
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-10-02 22:18:53 +0530

    update props in both parent and child

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,21 +1,51 @@
 import { DocIcon } from "../icons/DocIcon";
-import { HashTagIcon } from "../icons/HashTagIcon";
-import { LinkIcon } from "../icons/LinkIcon";
+import { ImageIcon } from "../icons/ImageIcon";
+import { SpeakerIcon } from "../icons/SpeakerIcon";
 import { TwitterIcon } from "../icons/TwitterIcon";
 import { YoutubeIcon } from "../icons/YoutubeIcon";
 import { Logo } from "./Logo";
 import { SideBarItem } from "./SideBarItem";
+// const contentType = ["image", "doc", "video", "audio", "tweet"];
 
-export function SideBar() {
+export function SideBar({
+  setVisibleContentType,
+}: {
+  setVisibleContentType: (e: string) => void;
+}) {
   return (
     <div>
       <Logo />
       <div className="mt-15">
-        <SideBarItem logo={<TwitterIcon />} name="Tweets" />
-        <SideBarItem logo={<YoutubeIcon />} name="Videos" />
-        <SideBarItem logo={<DocIcon />} name="Documents" />
-        <SideBarItem logo={<LinkIcon />} name="Links" />
-        <SideBarItem logo={<HashTagIcon />} name="Tags" />
+        <SideBarItem
+          type="tweet"
+          setVisibleContentType={setVisibleContentType}
+          logo={<TwitterIcon />}
+          name="Tweets"
+        />
+        <SideBarItem
+          type="video"
+          setVisibleContentType={setVisibleContentType}
+          logo={<YoutubeIcon />}
+          name="Videos"
+        />
+        <SideBarItem
+          type="doc"
+          setVisibleContentType={setVisibleContentType}
+          logo={<DocIcon />}
+          name="Documents"
+        />
+        <SideBarItem
+          type="image"
+          setVisibleContentType={setVisibleContentType}
+          logo={<ImageIcon />}
+          name="Images"
+        />
+        <SideBarItem
+          type="audio"
+          setVisibleContentType={setVisibleContentType}
+          logo={<SpeakerIcon />}
+          name="Audio"
+        />
       </div>
     </div>
   );
