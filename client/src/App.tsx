a0880537c0938dd82451d11050b6719e50ba9637
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-10-02 15:20:02 +0530

    add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import { Signin } from "./pages/Signin";
+import { Signup } from "./pages/Signup";
+
 export default function App() {
-  return <div>hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<Signin />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
