2fce842f67ccbbd571b0dc89d1e882fe80c2f7da
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-10-04 13:42:34 +0530

    update logic for searching sorting

--- server/src/controller/getContent.ts
+++ server/src/controller/getContent.ts
@@ -6,9 +6,23 @@ export async function getContent(req: Request, res: Response) {
   try {
     //@ts-ignore
     const userId = req.user.userId;
-    const contents = await contentModel.find({
-      userId,
-    });
+    const { search, sortBy, sortOrder } = req.query;
+    let query = { userId };
+    if (search) {
+      // @ts-ignore
+      query.$or = [
+        { title: { $regex: search, $options: "i" } },
+        { description: { $regex: search, $options: "i" } },
+      ];
+    }
+    let sort: any = {};
+    if (sortBy) {
+      sort[sortBy as string] = sortOrder === "desc" ? -1 : 1;
+    } else {
+      sort = { createdAt: -1 };
+    }
+
+    const contents = await contentModel.find(query).sort(sort).populate("tags");
     res.status(200).json({
       message: contents,
     });
