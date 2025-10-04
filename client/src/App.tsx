ac07b7e7fd414893ad48f63a5cb9a2347da5d012
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-10-04 19:10:59 +0530

    add default route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,7 @@ export default function App() {
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route element={<ProtectedRoute />}>
+          <Route path="/" element={<Dashboard />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
       </Routes>
