7c256b21e020e7bba74e11305e596d7fa18d6bc8
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-10-04 13:33:49 +0530

    change size

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,13 +1,22 @@
 import { Scholar } from "../icons/Scholar";
 
-export function Logo() {
+export function Logo({
+  setVisibleContentType,
+}: {
+  setVisibleContentType: (e: string) => void;
+}) {
+  function onclickhandler() {
+    setVisibleContentType("");
+  }
   return (
-    <div>
+    <div onClick={onclickhandler}>
       <div className="flex items-center">
-        <div className="text-xs text-purple-500">
-          <Scholar />
+        <div className="flex items-cener hover:cursor-pointer">
+          <div className="text-xs text-purple-500 ">
+            <Scholar />
+          </div>
+          <div className="ml-2 text-2xl font-bold">Brainly</div>
         </div>
-        <div className="ml-2 text-2xl font-bold">Brainly</div>
       </div>
     </div>
   );
